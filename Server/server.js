9a28d84537508fcae639ce652891297bc6e9cafa
Arjun Sundararajan <arjunsss@hotmail.com>
2013-12-18 17:53:47 -0800

    fixing port issue

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,7 @@
 var http = require('http');
-var port = proccess.env.PORT || 1337
+var port = process.env.PORT || 1338
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Hello Azure');
   }).listen(port);	
-console.log('Server running at http://127.0.0.1:1337/');
+console.log('Server running at http://127.0.0.1:' + port + '/');
