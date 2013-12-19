c3c94e201ba03f52650ddde1c70f014d78f23425
Arjun Sundararajan <arjunsss@hotmail.com>
2013-12-18 17:57:35 -0800

    adding server.js locally

---
+++ server.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var port = process.env.PORT || 1338
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello Azure');
+  }).listen(port);	
+console.log('Server running at http://127.0.0.1:' + port + '/');
