1735ce27c80967ed21846f5c6a9e280f36d0b372
Arjun Sundararajan <arjunsss@hotmail.com>
2013-12-18 17:50:02 -0800

    changing port number

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,7 @@
 var http = require('http');
+var port = proccess.env.PORT || 1337
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Hello Azure');
-  }).listen(1338, '127.0.0.1');	
+  }).listen(port);	
 console.log('Server running at http://127.0.0.1:1337/');
