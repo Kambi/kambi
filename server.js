8274a7f54b2598ef254f1c122b7cb0fbe3b26e52
Arjun Sundararajan <arjunsss@hotmail.com>
2013-12-18 18:04:18 -0800

    hello kambi

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require('http');
 var port = process.env.PORT || 1338
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello Azure');
+  res.end('Hello Thambi.. Welcome to Kambi');
   }).listen(port);	
 console.log('Server running at http://127.0.0.1:' + port + '/');
