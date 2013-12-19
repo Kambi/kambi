f485b433938e9d016c51ffbc92e77df39a0a73dd
Arjun Sundararajan <arjunsss@hotmail.com>
2013-12-18 23:24:12 -0800

    fixing service path

--- Server/service.js
+++ Server/service.js
@@ -1,7 +1,11 @@
 var http = require('http');
+
+exports.start = function() {
 var port = process.env.PORT || 1338
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello Azure');
+  res.end('Hello Thambi.. Welcome to Kambi');
   }).listen(port);	
-console.log('Server running at http://127.0.0.1:' + port + '/');
+
+	console.log('Server running at http://127.0.0.1:' + port + '/');
+}
