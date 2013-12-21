a0ec205e3220dc37614dfa4e1ad3357b02761a5e
Arjun Sundararajan <arjunsss@hotmail.com>
2013-12-20 16:05:09 -0800

    Movie

---
+++ routes/movies.js
@@ -0,0 +1,32 @@
+
+var request = require('request');
+var uri = 'http://kambidb.sb01.stations.graphenedb.com:24789/db/data/cypher';
+var localUri = 'http://localhost:7475/db/data/cypher';
+var authr = {'user': 'kambi-db', 'pass': 'rqs1jbbhQ109720ViAAl', 'sendImmediately':false };
+var getAllMovies = function(callback) {
+	request({
+	method: 'POST',
+	uri: uri,
+	body: '{ "query" : "MATCH (x:Movie) RETURN x.name as name, id(x) as id" }',
+	auth: authr
+	},
+	function(error, response, body) {
+		if(null == response)
+		{
+			console.log('Error');
+		}
+		else
+		{
+			console.log('Response status code : ' + response.statusCode);
+			callback(body);
+		}
+	}
+);
+};
+
+exports.list = function(request, response, next) {
+	getAllMovies( function(body) {
+		response.writeHead(200, {'Content-Type': 'text/html'});
+		response.end(body);
+	});
+};
