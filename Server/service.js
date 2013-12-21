8261bf6c5694f7c3a491c960b474040382234b8c
Arjun Sundararajan <arjunsss@hotmail.com>
2013-12-20 16:16:31 -0800

    updating service.js

--- Server/service.js
+++ Server/service.js
@@ -1,11 +1,27 @@
 var http = require('http');
+var express = require('express');
+var routes = require('../routes');
+
+var app = express();
 
 exports.start = function() {
-var port = process.env.PORT || 1338
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello Thambi.. Welcome to Kambi');
-  }).listen(port);	
 
-	console.log('Server running at http://127.0.0.1:' + port + '/');
-}
+app.set('port', process.env.PORT || 1338);
+app.set('views', __dirname + '/views');
+
+app.get('/', routes.movies.list);
+
+http.createServer(app).listen(app.get('port'), function() {
+	console.log('Express server listening on port' + app.get('port'));
+});
+
+};
+
+// exports.start = function() {
+// var port = process.env.PORT || 1338
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.end('Hello Thambi.. Welcome to Kambi');
+//   }).listen(port);	
+
+// }
